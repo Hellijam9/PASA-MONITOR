fce5f40b32480a767e8432ce2f29464d294084f8
Hellijam9 <hellijam9@gmail.com>
2025-06-24 18:44:18 +1000

    Update get_csv.py

--- get_csv.py
+++ get_csv.py
@@ -1,19 +1,23 @@
 import requests
 import zipfile
+import pandas as pd
 from io import BytesIO
 
 # Hardcoded URLs to download
 URL_OLD = "https://www.nemweb.com.au/REPORTS/CURRENT/MTPASA_DUIDAvailability/PUBLIC_MTPASADUIDAVAILABILITY_202506241500_0000000469074180.zip"
 URL_NEW = "https://www.nemweb.com.au/REPORTS/CURRENT/MTPASA_DUIDAvailability/PUBLIC_MTPASADUIDAVAILABILITY_202506241800_0000000469093579.zip"
 
-def download_zip(url):
+def extract_csv(url):
     print(f"Downloading: {url}")
     r = requests.get(url)
     r.raise_for_status()
     with zipfile.ZipFile(BytesIO(r.content)) as z:
         file_name = z.namelist()[0]
-        print(f"✅ Found file in ZIP: {file_name}")
+        print(f"✅ Extracting: {file_name}")
+        with z.open(file_name) as f:
+            df = pd.read_csv(f, skiprows=1, low_memory=False)
+            print(df.head())
 
 if __name__ == "__main__":
-    download_zip(URL_OLD)
-    download_zip(URL_NEW)
+    extract_csv(URL_OLD)
+    extract_csv(URL_NEW)
